9a5b7139f7102f700edf52473e9affbad15b6f07
Thomas Rust <rustthomasscol@gmail.com>
2022-11-15 18:39:59 +0400

    Push

--- src/tpTable2.java
+++ src/tpTable2.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class tpTable2 {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+
+        System.out.print("Entrer un nombre entier : ");
+        int nombre = ip.nextInt();
+
+        int k=nombre;
+        int factorielle=1;
+        System.out.printf("Factorielle de %d : ", nombre);
+        while(k>1) {
+            System.out.printf("%dx",k);
+            factorielle*=k;
+            k--;
+        }
+        System.out.printf("1 = %d",factorielle);
+
+
+    }
 }
