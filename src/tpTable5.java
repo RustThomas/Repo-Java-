fa72fb57bca316727177e5c637378722c06eea14
Thomas Rust <rustthomasscol@gmail.com>
2022-11-15 19:10:44 +0400

    Push

---
+++ src/tpTable5.java
@@ -0,0 +1,37 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class tpTable5 {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int n = rand.nextInt(99);
+        n+=1;
+
+        Scanner ip = new Scanner(System.in);
+
+        do {
+            System.out.print("Entrez un entier entre 1 et 100 : ");
+            int guess = ip.nextInt();
+
+            if (guess==n){
+                System.out.println("Trop fort !!!");
+                break;
+            }
+
+            else if (guess>n) {
+                System.out.println("Plus petit ^^");
+
+            }
+
+            else {
+                System.out.println("Plus grand ^^");
+
+            }
+
+
+        }while(true);
+    }
+
+}
