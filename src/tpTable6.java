e69f333ace9b833c84f4ba7c47e264cc53419630
Thomas Rust <rustthomasscol@gmail.com>
2022-11-16 13:13:51 +0400

    oubli/ajout cas multiple 3 et 5

--- src/tpTable6.java
+++ src/tpTable6.java
@@ -1,15 +1,23 @@
 public class tpTable6 {
     public static void main(String[] args) {
-        int n = 100;
+
 
         for (int i=1; i<=100 ; i++) {
-            if (i%3==0) {
+            if (i%3==0 && i%5!=0) {
                 System.out.println("fizz");
             }
 
             else if (i%5==0) {
-                System.out.println("buzz");
+                if (i%3==0){
+                    System.out.println("fizzbuzz");
+                }
+
+                else {
+                    System.out.println("buzz");
+                }
             }
+
+
 
             else {
                 System.out.println(i);
