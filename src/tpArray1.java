d1a47ddb72a47056de7a34d69df84ba7b253ace6
Thomas Rust <rustthomasscol@gmail.com>
2022-11-16 15:22:31 +0400

    Push

--- src/tpArray1.java
+++ src/tpArray1.java
@@ -18,18 +18,19 @@ public class tpArray1 {
 
 
         double notes[] = new double[length];
-        int k;
+
 
 // 2. Permettre à l'utilisateur d'entrer les notes du tableau à partir du clavier
 //        - Les valeurs ne doivent être qu'entre 0 et 20
 
         for (int i = 0; i < length; i++) {
+            int note;
             do {
-                System.out.printf("Entrez la %d-ième note : ", i+1);
-                k = input.nextInt();
+                System.out.printf("Entrez la %d-ième note (entre 0 et 20) : ", i+1);
+                note = input.nextInt();
 
-            } while (k < 0 || k > 20);
-            notes[i] = k;
+            } while (note < 0 || note > 20);
+            notes[i] = note;
         }
 // 3. Calculer et afficher la moyenne
         double somme = 0;
