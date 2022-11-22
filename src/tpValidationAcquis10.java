c348986f29e26d6119f8c3be9a221af480a54758
Thomas Rust <rustthomasscol@gmail.com>
2022-11-22 19:56:49 +0400

    Push

--- src/tpValidationAcquis10.java
+++ src/tpValidationAcquis10.java
@@ -1,25 +1,34 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class tpValidationAcquis10 {
 
-    final ArrayList<int[]> maMethode(int[] tableau, int S) {
+    static ArrayList<int[]> maMethode(int[] tableau, int S) {
 
         ArrayList<int[]> paires = new ArrayList<int[]>();
 
         HashMap<Integer,Integer> diff = new HashMap<Integer,Integer>();
         for(int i=0; i<tableau.length;i++) {
-            int delta =S-tableau[i];
+            int delta = S-tableau[i];
+            //System.out.println(delta);
 
 
-            if(diff.containsKey(delta)) {
-                int[] paire = new int[]{tableau[i],diff.get(delta)};
+            if(diff.containsKey(tableau[i])) {
+                int[] paire = new int[]{tableau[i],diff.get(tableau[i])};
                 paires.add(paire);
+                System.out.println("("+paire[0]+","+paire[1]+")");
             }
             else diff.put(delta, tableau[i]);
         }
-
+        //System.out.println(diff);
         return paires;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nombres = new int[]{2,7,11,15};
+        maMethode(nombres, 9);
     }
 
 }
